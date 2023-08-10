12e991b93a3cb04d1fe0709e1afd073172b63310
OmarAfet <OmarAlenezy2004@gmail.com>
2023-08-10 19:36:33 +0300

    change error page layout

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,7 @@
 const ErrorPage = () => {
   return (
     <>
-      <div className="text-center col-span-full mt-[25%] text-6xl font-bold">
+      <div className="text-center col-span-full mt-32 text-6xl font-bold">
         ERROR <span className="text-red-500">404</span>
       </div>
     </>
