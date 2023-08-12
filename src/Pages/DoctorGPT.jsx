66d70f6c3cfce11b468d4d4481e94f524a661bf0
OmarAfet <OmarAlenezy2004@gmail.com>
2023-08-12 08:32:36 +0300

    #

--- src/Pages/DoctorGPT.jsx
+++ src/Pages/DoctorGPT.jsx
@@ -10,7 +10,7 @@ const DoctorGPT = () => {
 
 	const openai = new OpenAIApi(
 		new Configuration({
-			apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+			apiKey: "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",
 		})
 	);
 
