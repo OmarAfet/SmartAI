ccd3ea26877f5b2ca8692687848d5975ad439dd8
OmarAfet <OmarAlenezy2004@gmail.com>
2023-08-14 06:42:34 +0300

    fix input height

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,7 +1,7 @@
 const Input = ({ name, classes, value, onBlur, onChange, type, placeholder, onKeyDown }) => {
 	return (
 		<>
-			<textarea autoComplete="off" value={value} title="" type={type} id={placeholder} onKeyDown={onKeyDown} onBlur={onBlur} onChange={onChange} className="flex-1 border-2 focus:border-main rounded-lg px-2 py-1 peer duration-0 h-[60px] min-h-[40px] max-h-[200px]" />
+			<textarea autoComplete="off" value={value} title="" type={type} id={placeholder} onKeyDown={onKeyDown} onBlur={onBlur} onChange={onChange} className="flex-1 border-2 focus:border-main rounded-lg px-2 py-1 peer duration-0 min-h-[40px] max-h-[200px]" />
 			<label htmlFor={placeholder} className={`absolute right-0 mx-[10px] px-1 text-gray-400 cursor-text inline-block peer-focus:-top-2.5 peer-focus:text-xs duration-150 ${value ? "text-xs -top-2.5" : "top-2"} bg-white select-none`}>{placeholder}</label>
 		</>
 	);
